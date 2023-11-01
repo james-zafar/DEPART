4a8d03f53709553e885075bdaeded14c7e5cf6ba
jameszafar <jzafar81@gmail.com>
2023-11-01 19:57:25 +0000

    Add Pylint, fix config warnings

--- app/model/model.py
+++ app/model/model.py
@@ -72,14 +72,14 @@ class DelayModel:
     @property
     def features(self) -> list[str]:
         return [
-            "OPERA_Latin American Wings",
-            "MES_7",
-            "MES_10",
-            "OPERA_Grupo LATAM",
-            "MES_12",
-            "TIPOVUELO_I",
-            "MES_4",
-            "MES_11",
-            "OPERA_Sky Airline",
-            "OPERA_Copa Air"
+            'OPERA_Latin American Wings',
+            'MES_7',
+            'MES_10',
+            'OPERA_Grupo LATAM',
+            'MES_12',
+            'TIPOVUELO_I',
+            'MES_4',
+            'MES_11',
+            'OPERA_Sky Airline',
+            'OPERA_Copa Air'
         ]
