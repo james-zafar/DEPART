5d8e845a86b0c040bf9fef9535ce46b28990b29a
jameszafar <jzafar81@gmail.com>
2023-11-05 17:32:34 +0000

    Add workaround for model deployed check

--- app/api/operations/get_model.py
+++ app/api/operations/get_model.py
@@ -19,6 +19,8 @@ async def get_model(model_id: uuid.UUID, request: Request, export: bool | None =
             return JSONResponse(content=new_error_response([ModelNotReadyError()]), status_code=ModelNotReadyError.status_code)
         file_name = file_name or f'{model_id}.pkl'
         model.model.save(file_name)
-    model_deployed = model_id == request.app.state.model.id
+    model_deployed = False
+    if request.app.state.model is not None:  # TODO: There should never not be a model
+        model_deployed = model_id == request.app.state.model.id
     response_json = model.new_model_response(deployed=model_deployed, exported=export)
     return JSONResponse(content=response_json, status_code=200)
